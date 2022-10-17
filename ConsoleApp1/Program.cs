a5f8103a70b53dcc4f2fac6162344f64dd2aa70a
SashaYaroshenko <yaroshenko2019ol@gmail.com>
2022-10-17 15:25:25 +0300

    Add VS project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Kata
+    {
+        public static string Rot13(string input)
+        {
+            string res = "";
+            foreach(char i in input)
+            {
+                if(Char.IsLetter(i))
+                {
+                    if(i > 64 && i < 78 || i > 96 && i < 110)
+                    {
+                        res += Convert.ToString(Convert.ToChar(i + 13));
+                    }
+                    else
+                    {
+                        res += Convert.ToString(Convert.ToChar(i - 13));
+                    }
+                }
+                else
+                {
+                    res += i.ToString();
+                }
+            }
+            return res;
+        }
+    }
+}
